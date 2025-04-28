582196254d2a7786a85f9d4972d48328cbf63710
Lucas Ezequiel Ojeda <luzojeda@proton.me>
2025-04-28 20:10:40 -0300

    add comments

--- nand2tetris2/module-4-5-compiler/project/JackTokenizer.js
+++ nand2tetris2/module-4-5-compiler/project/JackTokenizer.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const symbols = require("./symbols");
 
-// Removes all comments and white space from the input stream and breaks it into Jack-language tokens, as specified by the Jack grammar.
-
+/**
+ * Removes all comments and white space from the input stream and breaks it into Jack-language tokens.
+ * @param {string} source - Path to the source file to tokenize
+ * @returns {Array<string>} Array of tokens
+ */
 function tokenize(source) {
 	const content = fs.readFileSync(source, "utf8");
-
-	const cleanContents = clean(content);
-
+	const cleanContents = removeCommentsAndWhitespace(content);
 	const tokens = [];
 	let pointer = 0;
 	let currentToken = "";
@@ -15,14 +16,15 @@ function tokenize(source) {
 	while (pointer <= cleanContents.length - 1) {
 		let currentChar = cleanContents[pointer];
 
-		if (isSymbol(currentChar) || isIntegerConstant(currentChar)) {
+		/** symbol or digit */
+		if (isSymbol(currentChar) || isDigit(currentChar)) {
 			currentToken = currentChar;
 			pointer += 1;
 		} else if (currentChar === " ") {
 			// whitespace, move pointer to next position and add current token to tokens array
 			pointer += 1;
 		} else {
-			const result = handleStringConstant(cleanContents, pointer);
+			const result = extractStringOrIdentifier(cleanContents, pointer);
 			currentToken = result.value;
 			pointer += result.length;
 		}
@@ -36,8 +38,12 @@ function tokenize(source) {
 	return tokens;
 }
 
-function clean(content) {
-	/** Remove newlines and comments */
+/**
+ * Removes newlines and comments from the source content
+ * @param {string} content - Raw file content
+ * @returns {string} Cleaned content with comments and newlines removed
+ */
+function removeCommentsAndWhitespace(content) {
 	return content
 		.split("\n")
 		.map(line => line.split(/\/\/|\/\*\*/)[0].trim())
@@ -45,61 +51,98 @@ function clean(content) {
 		.join("");
 }
 
-function handleStringConstant(contents, startPosition) {
+/**
+ * Extracts either a string constant or identifier from the contents
+ * @param {string} contents - The cleaned source content
+ * @param {number} startPosition - Current position in the contents
+ * @returns {{value: string, length: number}} Extracted token and length consumed
+ */
+function extractStringOrIdentifier(contents, startPosition) {
 	const isStringConstant = contents[startPosition] === '"';
 	let currentPosition = startPosition;
-	let stringConstant = "";
+	let token = "";
 
 	if (isStringConstant) {
 		currentPosition += 1; // Skip past the opening quote
 		while (currentPosition < contents.length && contents[currentPosition] !== '"') {
-			stringConstant += contents[currentPosition];
+			token += contents[currentPosition];
 			currentPosition++;
 		}
-		currentPosition += 1; // SKip past the closing quote
+		currentPosition += 1; // Skip past the closing quote
 	} else {
 		while (
 			currentPosition < contents.length &&
 			contents[currentPosition] !== " " &&
 			!isSymbol(contents[currentPosition])
 		) {
-			stringConstant += contents[currentPosition];
+			token += contents[currentPosition];
 			currentPosition++;
 		}
 	}
 
 	const charsConsumed = currentPosition - startPosition;
-
 	return {
-		value: stringConstant,
+		value: token,
 		length: charsConsumed,
 	};
 }
 
-/** Returns true/false if character passed is any of
- * { | } | ( | ) | [ | ] | . | , | ; | + | - | * | / | & |
- * | | | < | > | = | -
+/**
+ * Checks if character is a symbol as defined in the Jack language
+ * @param {string} char - Character to check
+ * @returns {boolean} True if the character is a symbol
  */
 function isSymbol(char) {
 	return symbols.includes(char);
 }
 
-function isIntegerConstant(char) {
+/**
+ * Checks if character is a digit
+ * @param {string} char - Character to check
+ * @returns {boolean} True if the character is a digit
+ */
+function isDigit(char) {
 	return /^\d$/.test(char);
 }
 
-function getTokenType() {}
+/**
+ * Gets the type of the current token
+ * @returns {string} Token type
+ */
+function getTokenType() {
+	// Implementation needed
+}
 
-/** Only if current token is KEYWORD */
-function getKeyword() {}
+/**
+ * Gets the keyword value of the current token
+ * @returns {string} Keyword value
+ */
+function getKeywordValue() {
+	// Implementation needed
+}
 
-/** Only if current token is SYMBOL */
-function getSymbol() {}
+/**
+ * Gets the symbol value of the current token
+ * @returns {string} Symbol value
+ */
+function getSymbolValue() {
+	// Implementation needed
+}
 
-/** Only if current token is INT_CONST */
-function getIntValue() {}
+/**
+ * Gets the integer value of the current token
+ * @returns {number} Integer value
+ */
+function getIntValue() {
+	// Implementation needed
+}
 
-/** Only if current token is STRING_CONST */
-function getIntValue() {}
+/**
+ * Gets the string value of the current token
+ * @returns {string} String value
+ */
+function getStringValue() {
+	// Implementation needed
+}
 
 module.exports = { tokenize };
