6c22684d703dc33a07e96caac6c88917bfff3752
Lucas Ezequiel Ojeda <luzojeda@proton.me>
2025-05-02 14:45:03 -0300

    compile statements function and compile return statement

--- nand2tetris2/module-4-5-compiler/project/CompilationEngine.js
+++ nand2tetris2/module-4-5-compiler/project/CompilationEngine.js
@@ -47,7 +47,7 @@ function compileSubroutine(tokens, tab, pointer) {
 	xml += compileTerminalToken(tokens[pointer++], tab + 1); // ')'
 
 	// Subroutine body
-	const body = compileSubroutineBody(tokens, tab, pointer);
+	const body = compileSubroutineBody(tokens, tab + 1, pointer);
 	xml += body.xml;
 	pointer = body.pointer;
 
@@ -93,10 +93,15 @@ function compileSubroutineBody(tokens, tab, pointer) {
 	xml += compileTerminalToken(tokens[pointer++], tab + 1); // {
 
 	while (tokens[pointer]?.value === "var") {
+		// var declarations
 		const result = compileVarDec(tokens, tab + 1, pointer);
 		xml += result.xml;
 		pointer = result.pointer;
 	}
+
+	const statementsResult = compileStatements(tokens, tab + 1, pointer); // statements
+	xml += statementsResult.xml;
+	pointer = statementsResult.pointer;
 
 	xml += `${tabs}</subroutineBody>\n`;
 	return { xml, pointer };
@@ -136,25 +141,107 @@ function compileVarDec(tokens, tab, pointer) {
 	return { xml, pointer };
 }
 
-function compileStatements(tokens, tab) {}
+function compileStatements(tokens, tab, pointer) {
+	const tabs = "\t".repeat(tab);
+	let xml = `${tabs}<statements>\n`;
+
+	while (tokens[pointer].value !== "}") {
+		// check if current token is do, let, while, return or if and pass control to its compile function
+		switch (tokens[pointer].value) {
+			case "do":
+				const doResult = compileDo(tokens, tab + 1, pointer);
+				xml += doResult.xml;
+				pointer = doResult.pointer;
+				break;
+			case "let":
+				const letResult = compileLet(tokens, tab + 1, pointer);
+				xml += letResult.xml;
+				pointer = letResult.pointer;
+				break;
+			case "while":
+				const whileResult = compileWhile(tokens, tab + 1, pointer);
+				xml += whileResult.xml;
+				pointer = whileResult.pointer;
+				break;
+			case "return":
+				const returnResult = compileReturn(tokens, tab + 1, pointer);
+				xml += returnResult.xml;
+				pointer = returnResult.pointer;
+				break;
+			case "if":
+				const ifResult = compileIf(tokens, tab + 1, pointer);
+				xml += ifResult.xml;
+				pointer = ifResult.pointer;
+				break;
+		}
+	}
+	xml += `${tabs}</statements>\n`;
+	return { xml, pointer };
+}
+
+function compileDo(tokens, tab) {}
 
 function compileLet(tokens, tab) {}
 
-function compileExpression(tokens, tab) {}
+function compileWhile(tokens, tab) {}
+
+function compileReturn(tokens, tab, pointer) {
+	// 'return' expression? ';'
+	/**
+	 * Example: 
+	 * <returnStatement>
+          <keyword> return </keyword>
+          <symbol> ; </symbol>
+        </returnStatement>
+	 */
+	const tabs = "\t".repeat(tab);
+	let xml = `${tabs}<returnStatement>\n`;
+
+	xml += compileTerminalToken(tokens[pointer++], tab + 1); // 'return'
+
+	if (tokens[pointer].value !== ";") {
+		const expressionResult = compileExpression(tokens, tab, pointer);
+		xml += expressionResult.xml;
+		pointer += expressionResult.pointer;
+	}
+
+	xml += compileTerminalToken(tokens[pointer++], tab + 1); // ;
+	xml += `${tabs}</returnStatement>\n`;
+	return { xml, pointer };
+}
+
+function compileIf(tokens, tab) {}
+
+function compileExpression(tokens, pointer, tab) {
+	// term (op term)*
+	/**
+	 * <expression>
+            <term>
+              <identifier> Array </identifier>
+              <symbol> . </symbol>
+              <identifier> new </identifier>
+              <symbol> ( </symbol>
+              <expressionList>
+                <expression>
+                  <term>
+                    <identifier> length </identifier>
+                  </term>
+                </expression>
+              </expressionList>
+              <symbol> ) </symbol>
+            </term>
+          </expression>
+	 */
+	const tabs = "\t".repeat(tab);
+	let xml = `${tabs}<expression>\n`;
+
+	xml += `${tabs}</expression>\n`;
+
+	return { xml, pointer };
+}
 
 function compileTerm(tokens, tab) {}
 
 function compileExpressionList(tokens, tab) {}
-
-const tokenToTag = {
-	class: "class",
-	static: "classVarDec",
-	field: "classVarDec",
-	function: "subroutineDec",
-	do: "doStatement",
-	while: "whileStatement",
-	return: "returnStatement",
-	if: "ifStatement",
-};
 
 module.exports = { parseToXML };
